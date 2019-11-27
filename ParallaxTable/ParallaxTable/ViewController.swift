ac4af2035edb2ba7e6029018406fcf144ace75a8
DmitryDevDes <owndmitry111@gmail.com>
2019-11-27 21:14:18 +0200

    first commit

---
+++ ParallaxTable/ParallaxTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParallaxTable
+//
+//  Created by Дмитрий on 11/27/19.
+//  Copyright © 2019 OwnDmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
